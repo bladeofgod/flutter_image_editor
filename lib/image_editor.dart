1074df8b05ce996e0ce75d569b84e3cde1ef5aee
李佳奇 <450103187@qq.com>
2022-01-17 11:09:40 +0800

    fix: clear channel

--- lib/image_editor.dart
+++ lib/image_editor.dart
@@ -4,11 +4,5 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ImageEditor {
-  static const MethodChannel _channel =
-      const MethodChannel('image_editor');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
 }
