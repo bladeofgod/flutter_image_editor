53b1f45bf3b758a44b06c2dae79e196c200b26f3
李佳奇 <450103187@qq.com>
2022-01-21 11:45:00 +0800

    fix: add demo page

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,26 +1,24 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:image_editor/image_editor.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
     super.initState();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +27,69 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  File? _image;
+
+  final picker = ImagePicker();
+
+  Future<void> getimageditor(File origin) async {
+    return Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return ImageEditor(
+        originImage: origin,
+      );
+    })).then((geteditimage) {
+      if (geteditimage is EditorImageResult) {
+        setState(() {
+          _image = geteditimage.newFile;
+        });
+      }
+    }).catchError((er) {
+      debugPrint(er);
+    });
+  }
+
+  void getImage() async {
+    PickedFile? image =
+    await picker.getImage(source: ImageSource.gallery);
+    if(image != null) {
+      final File origin = File(image.path);
+      getimageditor(origin);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Container(
+        width: double.infinity, height: double.infinity,
+        child: Column(
+          children: [
+            if(_image != null)
+              Expanded(child: Image.file(_image!)),
+            ElevatedButton(
+              onPressed: getImage,
+              child: Text('edit image'),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget condition({required bool condtion, required Widget isTrue, required Widget isFalse}) {
+  return condtion ? isTrue : isFalse;
 }
