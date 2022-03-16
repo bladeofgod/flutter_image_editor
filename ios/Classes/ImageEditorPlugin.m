84c052b6e1a76e3bea1dc52e6abdd179aa169f6e
李佳奇 <450103187@qq.com>
2022-03-16 15:57:36 +0800

    fix: ios pod error

--- ios/Classes/ImageEditorPlugin.m
+++ ios/Classes/ImageEditorPlugin.m
@@ -1,11 +1,11 @@
 #import "ImageEditorPlugin.h"
 #if __has_include(<image_editor/image_editor-Swift.h>)
-#import <image_editor/image_editor-Swift.h>
+#import <image_editor_dove/image_editor_dove-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "image_editor-Swift.h"
+#import "image_editor_dove-Swift.h"
 #endif
 
 @implementation ImageEditorPlugin
